93ad0abd35d82dacd1b3067b437957c9d92cdf62
zhekshen <zhekshen@example.com>
2026-02-28 07:50:49 +0300

    Add DEFAULT_AUTO_FIELD setting to avoid primary key warnings

--- poshychemu_project/settings.py
+++ poshychemu_project/settings.py
@@ -123,5 +123,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
