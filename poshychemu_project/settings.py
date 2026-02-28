50c11ba815f1e9a0974f993c519d97c1af7c9ffa
zhekshen <zhekshen@example.com>
2026-02-28 04:51:53 +0300

    Fix ALLOWED_HOSTS for poshychemu username

--- poshychemu_project/settings.py
+++ poshychemu_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h3iv^@hkw+-v@btpl7&n+z=***d_t+2gsyzx772&meq(dyyq&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zhekshen.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['poshychemu.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
