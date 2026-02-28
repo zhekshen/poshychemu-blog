84d266e8691286bffaea236eb53ab75cc9820820
zhekshen <zhekshen@example.com>
2026-02-28 04:26:00 +0300

    Configure ALLOWED_HOSTS and STATIC_ROOT for deployment

--- poshychemu_project/settings.py
+++ poshychemu_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h3iv^@hkw+-v@btpl7&n+z=***d_t+2gsyzx772&meq(dyyq&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zhekshen.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static_root'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
